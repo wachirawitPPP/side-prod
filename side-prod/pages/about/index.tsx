95cdbbd38d731e42089d2497df481b7d7627ed0f
pertiiez <wachirawit.works@gmail.com>
2024-07-23 21:40:36 +0700

    update layout

--- side-prod/pages/about/index.tsx
+++ side-prod/pages/about/index.tsx
@@ -1,14 +1,15 @@
 import { title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
+import DefaultSidebarLayout from "@/layouts/side-bar/defualt";
 
 export default function DocsPage() {
   return (
-    <DefaultLayout>
+    <DefaultSidebarLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
           <h1 className={title()}>About</h1>
         </div>
       </section>
-    </DefaultLayout>
+    </DefaultSidebarLayout>
   );
 }
